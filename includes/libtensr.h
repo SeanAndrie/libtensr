55591cebcb86997ed53b46fb720584283c9c2729
SeanAndrie <gseanandrie@gmail.com>
2026-03-05 21:05:49 +0400

    refactor: moved callbacks into a single directory

--- includes/libtensr.h
+++ includes/libtensr.h
@@ -6,14 +6,14 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 14:48:59 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/03/02 15:54:50 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/03/05 20:30:26 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBTENSR_H
 # define LIBTENSR_H
 
-# include <core/tensr_debug.h>
+# include <utils/tensr_debug.h>
 # include <core/tensr_math.h>
 # include <core/tensr.h>
 
