2fb74d8153aae5322bc4ee8aeb6d09c8ffe4f3ef
SeanAndrie <gseanandrie@gmail.com>
2026-03-06 02:25:16 +0400

    feat: added tensr_sqrt.c

--- includes/core/tensr_math.h
+++ includes/core/tensr_math.h
@@ -6,25 +6,17 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 17:49:08 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/03/06 00:32:25 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/03/06 02:06:20 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TENSR_MATH_H
 # define TENSR_MATH_H
 
-typedef union u_result
-{
-	int					i;
-	long long			l;
-	float				f;
-	double				d;
-}						t_result;
-
 typedef struct s_tensr	t_tensr;
 
 // Elementwise Operations
-t_tensr                 *tensr_neg(const t_tensr *t);
+t_tensr					*tensr_neg(const t_tensr *t);
 t_tensr					*tensr_add(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_mul(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_sub(const t_tensr *a, const t_tensr *b);
@@ -39,6 +31,7 @@ t_tensr					*tensr_min(const t_tensr *t, const int n_axes,
 							const size_t *axes);
 
 // Linear Algebra Operations
+t_tensr					*tensr_sqrt(const t_tensr *t);
 t_tensr					*tensr_dot(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_cross(const t_tensr *a, const t_tensr *b);
 
