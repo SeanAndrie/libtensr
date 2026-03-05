6d26c31a5863f33dafe5d8f0b15037aed6db1ee3
SeanAndrie <gseanandrie@gmail.com>
2026-03-05 04:50:38 +0400

    fix: cross_entry dangling return value

--- src/math/linalg/tensr_cross.c
+++ src/math/linalg/tensr_cross.c
@@ -75,12 +75,13 @@ static void	*sub_func(void *a, void *b, t_dtype dtype)
 static void	*cross_entry(const t_tensr *a, const t_tensr *b,
 		const size_t *a_indices, const size_t *b_indices)
 {
-	void	*a_prod;
-	void	*b_prod;
+	static t_result	a_val;
+	void			*raw;
 
-	a_prod = mul_func(tensr_get(a, a_indices), tensr_get(b, b_indices), a->dtype);
-	b_prod = mul_func(tensr_get(a, b_indices), tensr_get(b, a_indices), b->dtype);
-	return (sub_func(a_prod, b_prod, a->dtype));
+	raw = mul_func(tensr_get(a, a_indices), tensr_get(b, b_indices), a->dtype);
+	ft_memcpy(&a_val, raw, a->elemsize);
+	return (sub_func(&a_val, mul_func(tensr_get(a, b_indices), tensr_get(b,
+					a_indices), a->dtype), a->dtype));
 }
 
 t_tensr	*tensr_cross(const t_tensr *a, const t_tensr *b)
