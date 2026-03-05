8b9172512ae784a2b8cedd84e631e89910c132a4
SeanAndrie <gseanandrie@gmail.com>
2026-03-05 22:04:40 +0400

    refactor: renamed cross_entry to set_cross_entry

--- src/math/linalg/tensr_cross.c
+++ src/math/linalg/tensr_cross.c
@@ -26,23 +26,25 @@ static void	acc_init(void *acc, t_dtype dtype)
 		*(double *)acc = 0.0;
 }
 
-static void	cross_entry(const t_tensr *a, const t_tensr *b, void *data, const size_t *indices)
+static void	set_cross_entry(const t_tensr *a, const t_tensr *b, void *data,
+		const size_t *indices)
 {
-    unsigned char a_v[MAX_ACC_SIZE];
-    unsigned char b_v[MAX_ACC_SIZE];
-    
-    acc_init(a_v, a->dtype);
-    acc_init(b_v, a->dtype);
+	unsigned char	a_v[MAX_ACC_SIZE];
+	unsigned char	b_v[MAX_ACC_SIZE];
 
-    mul_func(tensr_get(a, (size_t[]){indices[0]}), tensr_get(b, (size_t[]){indices[1]}), a_v, a->dtype);
-    mul_func(tensr_get(a, (size_t[]){indices[1]}), tensr_get(b, (size_t[]){indices[0]}), b_v, a->dtype);
-    sub_func(a_v, b_v, data, a->dtype);
+	acc_init(a_v, a->dtype);
+	acc_init(b_v, a->dtype);
+	mul_func(tensr_get(a, (size_t[]){indices[0]}), tensr_get(b,
+			(size_t[]){indices[1]}), a_v, a->dtype);
+	mul_func(tensr_get(a, (size_t[]){indices[1]}), tensr_get(b,
+			(size_t[]){indices[0]}), b_v, a->dtype);
+	sub_func(a_v, b_v, data, a->dtype);
 }
 
 /*
- * a[1]*b[2] - a[2]*b[1]
- * a[2]*b[0] - a[0]*b[2]
- * a[0]*b[1] - a[1]*b[0]
+ * a[1]*b[2] - a[2]*b[1] -> [[a]
+ * a[2]*b[0] - a[0]*b[2] ->  [b]
+ * a[0]*b[1] - a[1]*b[0] ->  [c]]
  */
 t_tensr	*tensr_cross(const t_tensr *a, const t_tensr *b)
 {
@@ -57,8 +59,8 @@ t_tensr	*tensr_cross(const t_tensr *a, const t_tensr *b)
 	out = tensr_alloc(1, (size_t[]){3}, a->dtype);
 	if (!out)
 		return (NULL);
-    cross_entry(a, b, tensr_get(out, (size_t[]){0}), (size_t[]){1, 2});
-    cross_entry(a, b, tensr_get(out, (size_t[]){1}), (size_t[]){2, 0});
-    cross_entry(a, b, tensr_get(out, (size_t[]){2}), (size_t[]){0, 1});
+	set_cross_entry(a, b, tensr_get(out, (size_t[]){0}), (size_t[]){1, 2});
+	set_cross_entry(a, b, tensr_get(out, (size_t[]){1}), (size_t[]){2, 0});
+	set_cross_entry(a, b, tensr_get(out, (size_t[]){2}), (size_t[]){0, 1});
 	return (out);
 }
