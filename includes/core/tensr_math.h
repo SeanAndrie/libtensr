d2fa1e5f1466a5316d59fc0f665a440c275d8a83
SeanAndrie <gseanandrie@gmail.com>
2026-03-08 20:41:19 +0400

    feat: added tensr_clamp

--- includes/core/tensr_math.h
+++ includes/core/tensr_math.h
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 17:49:08 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/03/06 13:55:57 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/03/08 20:40:27 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ t_tensr					*tensr_add(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_mul(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_sub(const t_tensr *a, const t_tensr *b);
 t_tensr					*tensr_div(const t_tensr *a, const t_tensr *b);
+t_tensr                 *tensr_clamp(const t_tensr *t, double min, double max);
 
 // Reductions Operations
 t_tensr					*tensr_sum(const t_tensr *t, const int n_axes,
