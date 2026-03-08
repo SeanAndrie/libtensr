f31779c02814f52450ade9104601b8523edcac0a
SeanAndrie <gseanandrie@gmail.com>
2026-03-08 16:54:28 +0400

    refactor: removed abs_func callback

--- src/math/elementwise/tensr_abs.c
+++ src/math/elementwise/tensr_abs.c
@@ -10,27 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include <core/tensr.h>
 #include <core/tensr_math.h>
-#include <utils/tensr_callbacks.h>
 
 t_tensr	*tensr_abs(const t_tensr *t)
 {
-	t_iter			it;
-	t_tensr			*out;
+    t_iter			it;
+    t_tensr			*out;
     void            *src;
     void            *dst;
 
-	if (!t)
-		return (NULL);
-	out = tensr_alloc(t->layout.ndim, t->layout.shape, t->dtype);
-	if (!out || !iter_init(&out->layout, &it))
-		return (NULL);
-	while (iter_next(&it))
-	{
+    if (!t)
+        return (NULL);
+    out = tensr_alloc(t->layout.ndim, t->layout.shape, t->dtype);
+    if (!out || !iter_init(&out->layout, &it))
+        return (NULL);
+    while (iter_next(&it))
+    {
         src = tensr_get(t, it.indices);
         dst = tensr_get(out, it.indices);
-        abs_func(src, dst, t->dtype);
-	}
-	return (out);
+        if (t->dtype == DT_I32)
+            *(int *)tensr_get(out, it.indices) = abs(*(int *)src);
+        else if (t->dtype == DT_I64)
+            *(long long *)dst = llabs(*(long long *)src);
+        else if (t->dtype == DT_F32)
+            *(float *)dst = fabsf(*(float *)src);
+        else if (t->dtype == DT_F64)
+            *(double *)dst = fabs(*(double *)src);
+    }
+    return (out);
 }
