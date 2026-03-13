2e7c686f47b0157b83b99fb755e95361c39729ef
SeanAndrie <gseanandrie@gmail.com>
2026-03-14 02:49:55 +0400

    refactor: allow to pass NULL in tensr_view to create pure views

--- src/tensr/view/tensr_view.c
+++ src/tensr/view/tensr_view.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 14:11:36 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/02/27 18:02:17 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/03/14 02:48:37 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ t_tensr	*tensr_view(const t_tensr *t, void *data, const t_layout *l)
 		return (NULL);
 	v->size = 1;
 	v->data = data;
-	v->dtype = t->dtype;
 	v->owns_data = false;
-	v->elemsize = t->elemsize;
+    if (t)
+    {
+        v->dtype = t->dtype;
+        v->elemsize = t->elemsize;
+    }
     i = 0;
     while (i < l->ndim)
         v->size *= l->shape[i++];
     if (!layout_alloc(l->ndim, &v->layout))
-    {
-        free(v);
-        return (NULL);
-    }
+        return (free(v), NULL);
     if (!layout_copy(&v->layout, l))
     {
         layout_free(&v->layout);
