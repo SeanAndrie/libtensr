357eed13e31ba5f6f1a368541604dfc8333ac327
SeanAndrie <gseanandrie@gmail.com>
2026-03-24 21:40:10 +0400

    chore: removed commented out implementation

--- src/tensr_core/tensor/tensr_offset.c
+++ src/tensr_core/tensor/tensr_offset.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 14:38:23 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/02/17 17:15:29 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/03/24 21:39:51 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,3 @@ size_t  tensr_offset(const t_layout *l, const size_t *indices)
     }
     return (offset);
 }
-
-// size_t	tensr_offset(const t_tensr *t, const size_t *indices)
-// {
-// 	int		    i;
-//     t_layout    l;
-// 	size_t	offset;
-//
-// 	if (!t || !indices)
-// 		return ((size_t)-1);
-// 	i = 0;
-// 	offset = 0;
-//     l = t->layout;
-// 	while (i < l.ndim)
-// 	{
-// 		if (indices[i] >= l.shape[i])
-// 			return ((size_t)-1);
-// 		offset += indices[i] * l.stride[i];
-// 		i++;
-// 	}
-// 	return (offset);
-// }
