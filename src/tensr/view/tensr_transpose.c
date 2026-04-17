dbb921c7b2edba4f90d5f0328d88b140e06a62f0
SeanAndrie <gseanandrie@gmail.com>
2026-04-17 12:59:34 +0400

    fix: returned tensor layout pointing to garbage

--- src/tensr/view/tensr_transpose.c
+++ src/tensr/view/tensr_transpose.c
@@ -16,20 +16,18 @@ t_tensr	*tensr_transpose(const t_tensr *t)
 {
 	t_tensr		*v;
 	t_layout	layout;
-	size_t		new_shape[2];
-	size_t		new_stride[2];
+	size_t		shape[2];
+	size_t		stride[2];
 
 	if (!t || t->layout.ndim != 2)
 		return (NULL);
-	new_shape[0] = t->layout.shape[1];
-	new_shape[1] = t->layout.shape[0];
-	new_stride[0] = t->layout.stride[1];
-	new_stride[1] = t->layout.stride[0];
-	ft_memset(&layout, 0, sizeof(layout));
-	layout.ndim = t->layout.ndim;
-	layout.shape = new_shape;
-	layout.stride = new_stride;
+	shape[0] = t->layout.shape[1];
+	shape[1] = t->layout.shape[0];
+	stride[0] = t->layout.stride[1];
+	stride[1] = t->layout.stride[0];
+	layout.ndim = 2;
+	layout.shape = shape;
+	layout.stride = stride;
 	v = tensr_view(t, t->data, &layout);
-	layout_free(&layout);
 	return (v);
 }
