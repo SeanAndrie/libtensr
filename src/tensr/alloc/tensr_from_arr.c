c229171359536c3de83880c79452646915e1278d
SeanAndrie <gseanandrie@gmail.com>
2026-04-19 21:53:51 +0400

    refactor: rename tensr_from_data to tensr_from_arr, add dtype validation

--- src/tensr/alloc/tensr_from_arr.c
+++ src/tensr/alloc/tensr_from_arr.c
@@ -1,36 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tensr_from_data.c                                  :+:      :+:    :+:   */
+/*   tensr_from_arr.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/26 22:33:23 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/04/19 18:50:51 by sgadinga         ###   ########.fr       */
+/*   Created: 2026/04/19 22:09:28 by sgadinga          #+#    #+#             */
+/*   Updated: 2026/04/19 22:10:52 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <tensr/tensr.h>
 
-t_tensr	*tensr_from_data(void *data, const int ndim, const size_t *shape,
+t_tensr	*tensr_from_arr(t_array *arr, const int ndim, const size_t *shape,
 		t_dtype dtype)
 {
 	t_tensr	*t;
-	t_array	arr;
 
-	if (!data || ndim <= 0 || !shape)
+	if (!arr || !arr->data || ndim <= 0 || !shape)
+		return (NULL);
+	if (arr->dtype != dtype)
 		return (NULL);
 	t = tensr_alloc(ndim, shape, dtype);
 	if (!t)
 		return (NULL);
-	if (t->dtype == DT_I32)
-		arr = arr_i32(data, t->size);
-	else if (t->dtype == DT_I64)
-		arr = arr_i64(data, t->size);
-	else if (t->dtype == DT_F32)
-		arr = arr_f32(data, t->size);
-	else if (t->dtype == DT_F64)
-		arr = arr_f64(data, t->size);
-	ft_memcpy(t->data, arr.data, t->elemsize * t->size);
+	ft_memcpy(t->data, arr->data, t->elemsize * t->size);
 	return (t);
 }
--- src/vec3/generic/vec3_to_tensr.c
+++ src/vec3/generic/vec3_to_tensr.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 21:41:17 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/04/19 19:06:14 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/04/19 22:10:22 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_tensr	*vec3_to_tensr(t_vec3 v, const int ndim, const size_t *shape)
 {
 	int		i;
 	size_t	size;
+	t_array	array;
 
 	if (!shape || ndim <= 0)
 		return (NULL);
@@ -26,5 +27,6 @@ t_tensr	*vec3_to_tensr(t_vec3 v, const int ndim, const size_t *shape)
 		size *= shape[i++];
 	if (size != 3)
 		return (NULL);
-	return (tensr_from_data((float []){v.x, v.y, v.z}, ndim, shape, DT_F32));
+	arr = arr_f32((float[]){v.x, v.y, v.z}, 3);
+	return (tensr_from_arr(&arr, ndim, shape, DT_F32));
 }
